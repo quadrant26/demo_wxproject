08a988611a15db11ddf1771850c2c29037e84c89
quadrant26 <quadrant26@163.com>
2017-06-23 18:07:43 +0800

    利用 Storage 进行收藏的操作( 添加与取消 )

--- pages/post/post-detail/post-detail.js
+++ pages/post/post-detail/post-detail.js
@@ -1,37 +1,58 @@
 var PostData = require("../../../data/posts-data.js");
 
 Page({
-  data: {},
-  onLoad: function () {
+    data: {
+        
+    },
+    onLoad: function (options) {
 
-    var postdate = {
-      date: "Sep 18 2016",
-      title: "正是虾肥蟹壮时",
-      imgSrc: "/images/post/crab.png",
-      avatar: "/images/avatar/1.png",
-      content: "菊黄蟹正肥，品尝秋之味。徐志摩把,“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
-      reading: "112",
-      collection: "96",
-      headImgSrc: "/images/post/crab.png",
-      author: "林白衣",
-      dateTime: "24小时前",
-      detail: "菊黄蟹正肥，品尝秋之味。徐志摩把“看初花的荻芦”和“到楼外楼吃蟹”并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，壳凸红脂块块香”；在《世说新语》里，晋毕卓更是感叹“右手持酒杯，左手持蟹螯，拍浮酒船中，便足了一生矣。”漫漫人生长路，美食与爱岂可辜负？于是作为一个吃货，突然也很想回味一下属于我的味蕾记忆。记忆中的秋蟹，是家人的味道，弥漫着浓浓的亲情。\n\n是谁来自山川湖海，却囿于昼夜，厨房与爱？ 是母亲，深思熟虑，聪明耐心。吃蟹前，总会拿出几件工具，煞有介事而乐此不疲。告诉我们螃蟹至寒，需要佐以姜茶以祛寒，在配备的米醋小碟里，亦添入姜丝与紫苏，前者驱寒后者增香。泡好菊花茶，岁月静好，我们静等。",
-      postId: 0,
-      music: {
-        url: "http://ws.stream.qqmusic.qq.com/C100003507bR0gDKBm.m4a?fromtag=38",
-        title: "夜夜夜夜-齐秦",
-        coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000"
-      }
-    };
+        var id = options.id;
+        this.data.currentPostId = id;
+        var postdate = PostData.postList[id];
+        this.setData({
+            postdate: postdate
+        });
 
-    this.setData({
-      postdate : postdate
-    });
+        // 获取收藏状态
+        var posts_collected = wx.getStorageSync("posts_collected");
+        console.log(posts_collected);
+        if (posts_collected ){
+            var collected = posts_collected[id];
+            this.setData({
+                collected: collected
+            });
+        }else{
+            var posts_collected = {};
+            posts_collected[id] = false;
+            wx.setStorageSync("posts_collected", posts_collected);
+        }
+    },
 
-    //this.data.postdate = postdate;
-    console.log(this.data.postdate);
-  },
-  onReady: function () {
-    console.log("页面渲染完成！");
-  }
+    onCollectionTap : function (event){
+        // var that = this;
+        // wx.getStorage({
+        //     key: 'posts_collected',
+        //     success: function(res) {
+        //         var posts_collected = res.data;
+        //         var post_collected = posts_collected[that.data.currentPostId];
+        //         post_collected = !post_collected;
+
+        //         console.log(post_collected);
+        //     },
+        // })
+
+        this.getPostCollectedSyc();
+    },
+
+    getPostCollectedSyc : function (event){
+        var postsCollected = wx.getStorageSync("posts_collected");
+        var postCollected = postsCollected[this.data.currentPostId];
+        console.log(postCollected);
+        postCollected = !postCollected;
+        postsCollected[this.data.currentPostId] = postCollected;
+        wx.setStorageSync("posts_collected", postsCollected)
+        this.setData({
+            collected: postCollected
+        })
+    }
 })

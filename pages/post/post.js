aaf5717175cc1a2efb3a83fb8bb3d59b45a5baf8
quadrant26 <quadrant26@163.com>
2017-06-28 11:06:34 +0800

    swiper 跳转

--- pages/post/post.js
+++ pages/post/post.js
@@ -10,11 +10,26 @@ Page({
 
         console.log("页面加载完成");
     },
+
     onPostTap: function (event) {
 
         var postId = event.currentTarget.dataset.postid;
         wx.navigateTo({
             url: 'post-detail/post-detail?id=' + postId
         });
+    },
+
+    onSwiperTap : function (event){
+
+        // target 和 currentTarget
+        // target指的是当前点击的组件 和currentTarget 指的是事件捕获的组件
+        // target这里指的是image，而currentTarget指的是swiper
+
+        var postId = event.target.dataset.postid;
+        console.log(postId);
+
+        wx.navigateTo({
+            url: 'post-detail/post-detail?id=' + postId,
+        })
     }
 })

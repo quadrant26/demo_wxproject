ec86343938e3307427d7a4fbe455a9cd44d9b675
quadrant26 <quadrant26@163.com>
2017-06-29 18:27:27 +0800

    获取正在热映的数据和时间戳设置防止过载

--- pages/movies/more-movie/more-movie.js
+++ pages/movies/more-movie/more-movie.js
@@ -1,5 +1,8 @@
 // pages/movies/more-movie/more-movie.js
+var Utils = require("../../../utils/utils.js");
 var app = getApp();
+
+console.log(Utils);
 
 Page({
 
@@ -11,7 +14,7 @@ Page({
         isEmpty : true
     },
     onLoad: function (options) {
-        console.log(options);
+
         var category = options.category;
         var database = app.globalData.dataBase;
         this.data.navigateTitle = category;
@@ -32,8 +35,48 @@ Page({
         wx.setNavigationBarTitle({
             title: this.data.navigateTitle
         });
-        this.data.requestUrl = dataUrl;
-
+        this.setData({
+            requestUrl: dataUrl
+        });
+        // this.data.requestUrl = dataUrl;
+        var moreTime = wx.getStorageSync("moretime");
+        var nowTime = Date.now();
+        
+        if (!moreTime || (moreTime && (nowTime - moreTime >= 1500000) )){
+            Utils.http(dataUrl, this.processDoubanData);
+            wx.setStorageSync("moretime", nowTime)
+        }else{
+            var data_douban = wx.getStorageSync("moredata");
+            this.processDoubanData(data_douban);
+        };
     },
+
+    processDoubanData: function (moviedata) {
+        
+        wx.setStorageSync("moredata", moviedata);
+        var movies = [];
+        for (var index in moviedata.subjects) {
+            var subject = moviedata.subjects[index];
+            var title = subject.title;
+            if (title.length >= 6) {
+                title = title.substring(0, 6) + "...";
+            };
+
+            var temp = {
+                stars: Utils.convertToStarsArray(subject.rating.stars),
+                title: title,
+                average: subject.rating.average,
+                coverageUrl: subject.images.large,
+                movieId: subject.id
+            };
+            movies.push(temp);
+        };
+
+        this.setData({
+            movies: movies
+        });
+
+        console.log(movies);
+    }
 
 })

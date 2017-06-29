d347a33350bd73fe5a337c12bf75de9ffa48f11d
quadrant26 <quadrant26@163.com>
2017-06-29 15:16:24 +0800

    跳转到tabBar 使用 switchTab()

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,14 @@
 // pages/index/index.js
 Page({
-  onTap : function (){
-    
-    // 可以返回上一页
-    // wx.navigateTo({
-    //   url: "/pages/post/post"
-    // })
-    wx.redirectTo({
-      url: "/pages/post/post"
-    })
-    
-  }
+    onTap: function () {
+
+        // 可以返回上一页
+        // wx.navigateTo({
+        //   url: "/pages/post/post"
+        // })
+
+        wx.switchTab({
+            url: "../post/post"
+        });
+    }
 })

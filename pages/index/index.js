99e26aa85976c7d3394c990b3d9518989db71aad
quadrant26 <quadrant26@163.com>
2017-06-30 18:59:51 +0800

    用户信息获取

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,16 @@
 // pages/index/index.js
 Page({
+    data : {
+        avatarUrl : "",
+        nickName : "",
+    },
+    onLoad : function (){
+        var UserInfo = wx.getStorageSync("userInfo");
+        this.setData(UserInfo);
+    },
+    onReady: function () {
+        console.log(13);
+    },
     onTap: function () {
 
         // 可以返回上一页

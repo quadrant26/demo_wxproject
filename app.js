13f0eb3f4293019964bc898e189f319da2fd18e3
quadrant26 <quadrant26@163.com>
2017-06-30 19:00:36 +0800

    用户登录信息获取

--- app.js
+++ app.js
@@ -2,6 +2,44 @@ App({
     globalData : {
         g_isPlayingMusic: false,
         g_currentMusicPostId: null,
-        dataBase: "https://api.douban.com"
+        dataBase: "https://api.douban.com",
+        appid : "",
+        secret: ""
+    },
+    onLaunch : function (){
+        var that = this;
+        wx.login({
+            success : function (res){
+                if (res.code) {
+                    wx.getUserInfo({
+                        success: function (res) {
+                            var objz = {};
+                            objz.avatarUrl = res.userInfo.avatarUrl;
+                            objz.nickName = res.userInfo.nickName;
+                            console.log(objz);
+                            wx.setStorageSync('userInfo', objz);//存储userInfo
+                        }
+                    });
+                    var d = that.globalData;//这里存储了appid、secret、token串
+                    var l = 'https://api.weixin.qq.com/sns/jscode2session?appid=' + d.appid + '&secret=' + d.secret + '&js_code=' + res.code + '&grant_type=authorization_code';
+                    console.log(l)
+                    wx.request({
+                        url: l,
+                        data: {},
+                        method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+                        // header: {}, // 设置请求的 header
+                        success: function (res) {
+                            var obj = {};
+                            obj.openid = res.data.openid;
+                            obj.expires_in = Date.now() + res.data.expires_in;
+                            console.log(res);
+                            wx.setStorageSync('user', obj);//存储openid
+                        }
+                    });
+                } else {
+                    console.log('获取用户登录态失败！' + res.errMsg)
+                };
+            }
+        })
     }
 })
